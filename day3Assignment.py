5ef006054f188cbf14216cd2dcc8c51b98678626
Chanda87 <98538793+Chanda87@users.noreply.github.com>
2023-01-10 21:09:17 +0530

    Day3 assignment submitted

---
+++ day3Assignment.py
@@ -0,0 +1,11 @@
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\n")
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("* ", end=" ")
+
+    print("\n")
